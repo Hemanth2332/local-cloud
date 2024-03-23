15cb61b28c0afbdc68af9600674703539ccf0d02
Hemanth2332 <ktrhemanthh@gmail.com>
2024-03-23 20:22:47 +0530

    updated the webui.sh file

--- webui.sh
+++ webui.sh
@@ -5,51 +5,48 @@ echo "====================================================================="
 if [[ $VIRTUAL_ENV ]]
 then
     echo "virtual environment is activated"
-    python3 webapp/app.py
+    echo "======================================================================"
 
+        if command -v nvidia-smi &> /dev/null
+        then
+                echo 'nvidia driver is installed'
+
+                img_check=$(docker image ls| grep tensorflow- | awk '{ print $1 }')
+
+                if [[ "$img_check" == "tensorflow-" ]]
+                then
+                        echo "tensorflow image built exists"
+                else
+                        echo "tensorflow  is not built"
+                        echo "building the GPU tensorflow image"
+                        echo "--------------------------------------------------------"
+                        docker build -t tensorflow-2.10.1  --target tfgpu -f tensorflow.dockerfile .
+
+                fi
+
+        echo "======================================================================"
+
+        else
+                echo 'nvidia driver is not installed'
+
+                img_check=$(docker image ls| grep tensorflow- | awk '{ print $1 }')
+
+                if [[ "$img_check" == "tensorflow-" ]]
+                then
+                        echo "tensorflow image built exists"
+                else
+                        echo "tensorflow  is not built"
+                        echo "building the CPU tensorflow image"
+                        echo "--------------------------------------------------------"
+                        docker build -t tensorflow-2.10.1 --target tfcpu -f tensorflow.dockerfile .
+
+                fi
+
+        fi
 else
     echo "virtual environment directory is not found"
+    echo "====================================================================="
     exit
 fi
 
-echo "======================================================================"
-
-if command -v nvidia-smi &> /dev/null
-then
-        echo 'nvidia driver is installed'
-
-        img_check=$(docker image ls| grep tensorflow- | awk '{ print $1 }')
-
-        if [[ "$img_check" == "tensorflow-" ]]
-        then
-                echo "tensorflow image built exists"
-        else
-                echo "tensorflow  is not built"
-                echo "building the GPU tensorflow image"
-                echo "--------------------------------------------------------"
-                docker build -t tensorflow-2.10.1  --target tfgpu -f tensorflow.dockerfile .
-
-        fi
-
-echo "======================================================================"
-
-else
-        echo 'nvidia driver is not installed'
-
-        img_check=$(docker image ls| grep tensorflow- | awk '{ print $1 }')
-
-        if [[ "$img_check" == "tensorflow-" ]]
-        then
-                echo "tensorflow image built exists"
-        else
-                echo "tensorflow  is not built"
-                echo "building the CPU tensorflow image"
-                echo "--------------------------------------------------------"
-                docker build -t tensorflow-2.10.1 --target tfcpu -f tensorflow.dockerfile .
-
-        fi
-
-fi
-
-echo "====================================================================="
 
